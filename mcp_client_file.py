2d059405100565897c46510a1d61f35e99927a69
Aditya Bharat Soni <aditya.soni@nutanix.com>
2025-05-27 15:01:30 -0700

    minor fixes to mcp client, code works now

--- mcp_client_file.py
+++ mcp_client_file.py
@@ -49,7 +49,7 @@ class FileViewerClient:
         """
         # Initialize the client session
         result = await self.session.call_tool(name="list_files", arguments={"path": path})
-        return result
+        return result.content[0].text
 
     async def read_file(self, path: str) -> str:
         """Send a read file request to the MCP server.
@@ -62,7 +62,7 @@ class FileViewerClient:
 
         """
         result = await self.session.call_tool(name="read_file", arguments={"path": path})
-        return result   
+        return result.content[0].text   
 
     async def write_file(self, path: str, content: str) -> str:
         """Send a write file request to the MCP server.
@@ -75,7 +75,7 @@ class FileViewerClient:
             Success/error message as a string.
         """
         result = await self.session.call_tool(name="write_file", arguments={"path": path, "content": content})
-        return result
+        return result.content[0].text
 
     async def delete_file(self, path: str) -> str:
         """Send a delete file request to the MCP server.
@@ -87,7 +87,7 @@ class FileViewerClient:
             Success/error message as a string.
         """
         result = await self.session.call_tool(name="delete_file", arguments={"path": path})
-        return result
+        return result.content[0].text
 
 async def main():
     # Create client
@@ -99,10 +99,10 @@ async def main():
     result = await client.read_file(path = "/home/ubuntu/aditya/mcp-file-server/smolagents/examples/sandboxed_execution.py")
     print("Read File Result:", result)
 
-    result = await client.write_file(path = "/home/ubuntu/aditya/mcp-file-server/smolagents/examples2/test_write.txt", content = "This is a test write operation.")
+    result = await client.write_file(path = "/home/ubuntu/aditya/mcp-file-server/smolagents/SECURITY.md", content = "This is a test write operation.")
     print("Write File Result:", result)
 
-    result = await client.delete_file("/home/ubuntu/aditya/mcp-file-server/smolagents/docs/README.md")
+    result = await client.delete_file("/home/ubuntu/aditya/mcp-file-server/smolagents/docs/")
     print("Delete File Result:", result)
     await client.cleanup()
 
