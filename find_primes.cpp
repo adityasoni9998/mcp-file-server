68f47a1b799ab556cc14b325150a282f00a62ffb
Arshika Lalan <arshika.lalan@nutanix.com>
2025-05-28 00:08:29 +0000

    minor bug fix to prime finder cpp code

--- find_primes.cpp
+++ find_primes.cpp
@@ -1,13 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
-const ll INF = 1e9+7;
+const ll INF = 1e6;
 
-int fn(ll x = INF){
+//ground truth: https://t5k.org/howmany.html
+
+ll fn(ll x = INF){
     vector<bool> is_prime(x+1, 1);
     is_prime[0] = is_prime[1] = false;
     ll ans = 0;
     for(ll i=2;i<=x;i++){
+        // cout << i << endl;
         if(!is_prime[i])continue;
         ans += 1;
         for(ll j=2;j*i<=x;j++){
